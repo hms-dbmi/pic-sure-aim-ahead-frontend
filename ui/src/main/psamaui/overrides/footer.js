1126b74af7a7e7d23045a10f83a0c4fb9843f535
GeorgeC <gcolon021@gmail.com>
2024-01-02 14:55:29 -0500

    Remove accidentally duplicated files Remove unnecessary files

--- ui/src/main/psamaui/overrides/footer.js
+++
@@ -1,8 +0,0 @@
-define(["handlebars", "text!overrides/footer.hbs"], function(HBS, template){
-	return {
-		/*
-		 * The render function for the footer can be overridden here.
-		 */
-		render : undefined
-	};
-});
