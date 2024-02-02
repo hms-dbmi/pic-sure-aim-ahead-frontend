87699ad407c2cda2c2d0a533b700f17cd5693012
Gcolon021 <34667267+Gcolon021@users.noreply.github.com>
2024-02-02 09:51:41 -0500

    Initial aim ahead development (#3)
    * [ALS-5567] Update displayNotAuthorized function in login.js
    The displayNotAuthorized function in login.js has been updated to include a dynamic login link to the notAuthorizedTemplate. This change improves maintainability and decreases potential for errors as URLs won't require hard coding anymore.
    
    * [ALS-5566] Add showLoginPage function in login.js
    A new function showLoginPage has been added to login.js that displays a confirmation message upon successful authentication. This is specifically implemented for BioDataCatalyst authentication and the message is shown during the UserProfile information processing phase.
    
    * [GC] Remove unnecessary override files
    * [GC] Add logo to images directory in Dockerfile
    * [GC] Remove Open Access wording
    
    * [GC] Update header links in interface
    The href attribute in the interface header links has been updated to provide proper redirection functionality. This change corrects problems with dead links on the website, specifically in the header section, improving usability and the overall user experience.
    
    * [ALS-5517] Remove unused functions in router.js
    The commit removes displayDataAccess, getGenomicFilters, and displayAPI functions from router.js. These functions were not in use, contributing to complexity and potential confusion. The removal of these functions simplifies the codebase and makes maintenance easier.
    
    * Remove Open PIC-SURE link from header
    The Open PIC-SURE link in the header navigation has been removed as it was no longer needed. This change simplifies the header component and removes potential confusion for users navigating the UI.
    
    * Add landing page and update header in PIC-SURE UI
    * [ALS-5581] Remove footer content with override
    
    * Added a function to generate random state for each authentication request in login.js to mitigate CSRF attacks. Adjusted behavior when a code is present and added checks for state parameter to further secure the login flow. Also improved error message handling for authentication failures.
    
    * Add Super Admin and Admin navigation options
    A Super Admin Console drop-down menu and an Admin Users button have been introduced in the header navigation. The Super Admin Console contains links for Application, Connection, Privilege, and Role management. These functionalities are only visible to users with respective Super Admin and Admin privileges.
    
    * [ALS-5779] AIM-AHEAD PIC-SURE: Change header name from Authorized Access to Explore
    * [ALS-5779] Redirect to login page if authentication was successful
    * [ALS-5781] AIM-AHEAD PIC-SURE: Show the API tab
    * [ALS-5800] Set logo as favicon
    * [ALS-5781] AIM-AHEAD PIC-SURE: Show the API tab
    * [ALS-5567] Fix order and now hide header-tabs during the login process
    
    * Remove console log and inline redirect

--- ui/src/main/picsureui/overrides/router.js
+++ ui/src/main/picsureui/overrides/router.js
@@ -1,54 +1,23 @@
-define(["backbone", "underscore", "handlebars", "studyAccess/studyAccess", "picSure/settings", "filter/filterList",
+define(["backbone", "handlebars", "studyAccess/studyAccess", "picSure/settings", "filter/filterList",
         "openPicsure/outputPanel", "picSure/queryBuilder", "text!openPicsure/searchHelpTooltipOpen.hbs", "overrides/outputPanel",
         "search-interface/filter-list-view", "search-interface/search-view", "search-interface/tool-suite-view",
         "search-interface/query-results-view", "api-interface/apiPanelView", "search-interface/filter-model",
         "search-interface/tag-filter-model", "landing/landing", "common/session"],
-    function (BB, _, HBS, studyAccess, settings, filterList,
+    function (Backbone, HBS, studyAccess, settings, filterList,
               outputPanel, queryBuilder, searchHelpTooltipTemplate, output,
               FilterListView, SearchView, ToolSuiteView, queryResultsView,
               ApiPanelView, filterModel, tagFilterModel, landingView, session) {
-        let createUserSession = function (that, callback, args) {
-            let uuid = localStorage.getItem('OPEN_ACCESS_UUID');
-            if (uuid) {
-                uuid = JSON.parse(uuid);
-            }
 
-            $.ajax({
-                url: '/psama/open/authentication',
-                type: 'POST',
-                data: JSON.stringify({
-                    UUID: uuid
-                }),
-                contentType: 'application/json',
-                success: function (data) {
-                    if (data.uuid) {
-                        // we need to set the UUID cookie here, because the backend will not do it for us.
-                        localStorage.setItem('OPEN_ACCESS_UUID', JSON.stringify(data.uuid));
-                    }
-
-                    session.sessionInit(data);
-                    that.renderHeaderAndFooter();
-                    if (callback) {
-                        callback.apply(that, args);
-                    }
-                },
-                error: function (data) {
-                    // handle error
-                    console.log(data);
+        let getInvalidActiveFilters = function () {
+            const session = JSON.parse(sessionStorage.getItem("session"));
+            return filterModel.get('activeFilters').filter(filter => {
+                if (filter.get('type') === 'genomic') {
+                    return session.queryScopes && !session.queryScopes.includes('Gene_with_variant');
+                } else {
+                    const filterStudyId = '\\' + filter.get('searchResult').result.metadata.columnmeta_study_id + '\\';
+                    return session.queryScopes && !session.queryScopes.includes(filterStudyId);
                 }
             });
-        };
-
-        let execute = function (callback, args, name) {
-            let deferred = $.Deferred();
-            if (!session.isValid(deferred)) {
-                createUserSession(this, callback, args);
-            } else {
-                this.renderHeaderAndFooter();
-                if (callback) {
-                    callback.apply(this, args);
-                }
-            }
         };
 
         let displayLandingPage = function () {
@@ -59,58 +28,6 @@ define(["backbone", "underscore", "handlebars", "studyAccess/studyAccess", "picS
             const landing = new landingView({totalVars: totalVars});
             $('#main-content').append(landing.$el);
             landing.render();
-        };
-
-        let displayDataAccess = function () {
-            $(".header-btn.active").removeClass('active');
-            $(".header-btn[data-href='/picsureui/dataAccess']").addClass('active');
-            $('#main-content').empty();
-
-            var studyAccessView = new studyAccess.View();
-            $('#main-content').append(studyAccessView.$el);
-            studyAccessView.render();
-        };
-
-        let displayOpenAccess = function () {
-            sessionStorage.setItem("isOpenAccess", true);
-            BB.pubSub.trigger('destroySearchView');
-
-            $(".header-btn.active").removeClass('active');
-            $(".header-btn[data-href='/picsureui/openAccess#']").addClass('active');
-            $('#main-content').empty();
-            $('#main-content').append(this.layoutTemplate(settings));
-            let toolSuiteView = new ToolSuiteView({
-                el:
-                    $('#tool-suite-panel')
-                , isOpenAccess: true
-            });
-            toolSuiteView.render();
-
-            const outputPanelView = new outputPanel.View({toolSuiteView: toolSuiteView});
-            const query = queryBuilder.generateQueryNew({}, {}, null, settings.openAccessResourceId);
-            outputPanelView.render();
-            $('#query-results').append(outputPanelView.$el);
-
-            const parsedSess = JSON.parse(sessionStorage.getItem("session"));
-            if (parsedSess.queryTemplate === undefined) {
-                parsedSess.queryTemplate = "{}";
-            }
-
-            const searchView = new SearchView({
-                queryTemplate: JSON.parse(parsedSess.queryTemplate),
-                queryScopes: parsedSess.queryScopes,
-                el: $('#filter-list')
-            });
-
-            if ($('#search-results-panel').is(":visible")) {
-                $('#guide-me-button-container').hide();
-            }
-
-            const filterListView = new FilterListView({
-                outputPanelView: outputPanelView,
-                el: $('#filter-list-panel')
-            });
-            filterListView.render();
         };
 
         let displayAPI = function () {
@@ -132,23 +49,69 @@ define(["backbone", "underscore", "handlebars", "studyAccess/studyAccess", "picS
                  * Ex:
                  * "picsureui/queryBuilder2" : function() { renderQueryBuilder2(); }
                  */
-                "psamaui/login(/)": undefined,
-                "picsureui/login(/)": undefined,
-                "psamaui/logout(/)": undefined,
-                "picsureui/dataAccess": displayDataAccess,
-                "picsureui/openAccess": function () {
-                    displayOpenAccess.call(this);
-                },
+                "picsureui/dataAccess": undefined,
                 "picsureui/queryBuilder(/)": function () {
-                    displayOpenAccess.call(this);
+                    sessionStorage.setItem("isOpenAccess", false);
+                    let antiScopes = getInvalidActiveFilters();
+                    if (antiScopes && antiScopes.length > 0) {
+                        if (confirm('Filters on studies you are not authorized to access will be removed as they are not supported in Authorized Access. Are you sure you would like to proceed to Authorized Access?')) {
+                            filterModel.get('activeFilters').remove(antiScopes, {silent: true});
+                            antiScopes.forEach(filter => {
+                                tagFilterModel.removeRequiredTag(filter.get('searchResult').result.metadata.columnmeta_study_id);
+                                tagFilterModel.removeExcludedTag(filter.get('searchResult').result.metadata.columnmeta_study_id);
+                            });
+                        } else {
+                            this.navigate('picsureui/openAccess#', {trigger: true, replace: false});
+                            return;
+                        }
+                    }
+                    Backbone.pubSub.trigger('destroySearchView');
+                    $(".header-btn.active").removeClass('active');
+                    $(".header-btn[data-href='/picsureui/queryBuilder']").addClass('active');
+
+                    $('#main-content').empty();
+                    $('#main-content').append(this.layoutTemplate(settings));
+                    const toolSuiteView = new ToolSuiteView({
+                        el: $('#tool-suite-panel'),
+                        isOpenAccess: false
+                    });
+                    const queryView = new queryResultsView.View({
+                        model: new queryResultsView.Model(),
+                        toolSuiteView: toolSuiteView
+                    });
+
+                    queryView.render();
+                    $('#query-results').append(queryView.$el);
+
+                    const parsedSess = JSON.parse(sessionStorage.getItem("session"));
+
+                    const query = queryBuilder.generateQueryNew({}, {}, JSON.parse(parsedSess.queryTemplate), settings.picSureResourceId);
+
+                    const searchView = new SearchView({
+                        queryTemplate: JSON.parse(parsedSess.queryTemplate),
+                        queryScopes: parsedSess.queryScopes,
+                        el: $('#filter-list')
+                    });
+
+                    $('#studies-list-panel').remove();
+
+                    if ($('#search-results-panel').is(":visible")) {
+                        $('#guide-me-button-container').hide();
+                    }
+
+                    const filterListView = new FilterListView({
+                        outputPanelView: queryView,
+                        el: $('#filter-list-panel')
+                    });
+
+                    filterListView.render();
+
+                    toolSuiteView.render();
                 },
                 "picsureui/api": displayAPI,
                 "picsureui(/)": displayLandingPage,
             },
             defaultAction: displayLandingPage,
-            execute: function (callback, args, name) {
-                execute.call(this, callback, args, name);
-            }
         };
     }
 );
