a12769460c5cd2b6c60a16d21b563bcb6a845473
Gcolon021 <34667267+Gcolon021@users.noreply.github.com>
2024-02-06 10:26:22 -0500

    [ALS-5852] AIM-AHEAD PIC-SURE StatViz should work and not show obfuscation (#4)
    * [ALS-5852] Update Wording
    
    * [ALS-5852] Update footer render function
    
    The update introduces new dependencies in the footer.js file and modifies the render function with additional functionality. A new middleware and redirect modal are instantiated in the function. An event handler is also added for clicks on anchors with target="_blank" to prevent the default behavior and render the redirect modal instead.

--- ui/src/main/picsureui/overrides/footer.js
+++ ui/src/main/picsureui/overrides/footer.js
@@ -1,9 +1,25 @@
-define(["handlebars"], function(HBS){
-	return {
-		/*
-		 * The render function for the footer can be overridden here.
-		 */
-		
-		render : undefined
-	};
-});
+define(["handlebars",
+        "text!overrides/footer.hbs",
+        "common/modal",
+        "common/session",
+        "common/pic-sure-dialog-view",
+        "middleware/middleware",
+        "common/redirect-modal"
+    ],
+    function (HBS, template, modal, session, dialog, Middleware, redirectModal) {
+        return {
+            /*
+             * The render function for the footer can be overridden here.
+             */
+            render: function () {
+                new Middleware();
+                let redirect = new redirectModal();
+
+                // Using .off() to prevent multiple event handlers from being attached
+                $(document).off('click', 'a[target="_blank"]').on('click', 'a[target="_blank"]', function (event) {
+                    event.preventDefault();
+                    redirect.render(event);
+                });
+            }
+        };
+    });
