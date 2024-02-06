eb179dfaae0483df326f566ebbeda8ed5acb61a7
Gcolon021 <34667267+Gcolon021@users.noreply.github.com>
2024-02-06 10:12:45 -0500

    [ALS-5476] Add header.js override file to fix TOS (#8)
    * [ALS-5476] Add new header.js file for custom UI component
    This commit adds a new header file in the pic-sure-aim-ahead-frontend project to override the default UI and add custom behavior after the main header has rendered. Specifically, it introduces logic to focus on the help dropdown menu when the terms of service modal is displayed.
    
    * Refactor header.js to remove unnecessary import
    In the header.js file, the "header/goToAuthorized" import was removed as it was unnecessary. By doing so, the code becomes cleaner and easier to maintain.

---
+++ ui/src/main/picsureui/overrides/header.js
@@ -0,0 +1,26 @@
+define(["header/termsOfService", "common/modal"], function (termsOfService, modal) {
+    return {
+        /*
+         * The path to a logo image incase you don't want the default PrecisionLink one.
+         *
+         * This should be a String value.
+         */
+        logoPath: undefined,
+
+        /*
+         * This is used to add extra logic after the main header has rendered
+         */
+        renderExt: function () {
+            $(document).on('click', '#open-tos-modal', function (event) {
+                modal.displayModal(
+                    new termsOfService(),
+                    "NHLBI BioData Catalyst® (BDC) Powered by Open PIC-SURE Terms of Use",
+                    function () {
+                        // Focus the help dropdown menu
+                        $("#help-dropdown-toggle").focus();
+                    },
+                    {width: "90em", isHandleTabs: true});
+            });
+        }
+    };
+});
